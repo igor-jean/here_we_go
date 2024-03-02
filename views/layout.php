14f615b97a6856653e059dd50886afdf6ad0a692
Igor Jean <igorjean79@gmail.com>
2024-03-02 12:45:17 +0100

    Ajout du style fait par Lolita - mise en place de Sass

--- views/layout.php
+++ views/layout.php
@@ -12,7 +12,7 @@ ob_start();
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link href="css/styles.css" rel="stylesheet" />
+  <link href="styles/styles.css" rel="stylesheet" />
   <title>Here we go</title>
 </head>
 <body>
