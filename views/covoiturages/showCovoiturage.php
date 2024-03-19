c4b4c00a551a0718ec912c292b59ef2712ec6647
Lolital.gane71@gmail.com <you@example.com>
2024-03-19 16:22:42 +0100

    modif

--- views/covoiturages/showCovoiturage.php
+++ views/covoiturages/showCovoiturage.php
@@ -1,11 +1,11 @@
 <div class="container">
-<h1>Covoiturage</h1>
+<h1 class=mt-5>Covoiturage</h1>
 
 <p>Trajet proposé par <?php echo $conducteur;?></p>
 <ul>
-    <li>places disponnibles :<?php echo $covoit->getNbPlace();?></li>
+    <li>Places disponnibles :<?php echo $covoit->getNbPlace();?></li>
     <li>Prix par personnes: <?php echo $covoit->getMontantParPers();?></li>
-    <li>Adresse de depart :<?php echo $covoit->getLieuDepart();?></li>
+    <li>Adresse de depart : <?php echo $covoit->getLieuDepart();?></li>
     <li>Heure de depart :<?php echo $covoit->getHeureDepart();?></li>
 </ul>
 <p>information de contact :<?php echo $covoit->getInformationDeContact();?></p>
@@ -13,11 +13,12 @@
 <p>Vehicule du capitaine : <?php echo $vehicule->getLibelle_vehicule();?></p>
 
 
-<a href="?controller=evenements&action=showEvent&id_event=<?php echo $covoit->getIdEvent();?>">Retour</a>
+<a class= "btn btn-primary mb-5" href="?controller=evenements&action=showEvent&id_event=<?php echo $covoit->getIdEvent();?>">Retour</a>
 <?php if($inscrit) {
     echo '<a href="?controller=covoiturages&action=desinscriptionCovoiturage&id_covoiturage='.$covoit->getIdCovoiturage().'">Annuler la reservation</a>';
 }elseif(!$verif) {
     echo '<a href="?controller=covoiturages&action=inscriptionCovoiturage&id_covoiturage='.$covoit->getIdCovoiturage().'">Reserver</a>';
 }
 ?>
+<img src="assets\img\carpool-5508006_1280.png"></a>
 </div>
