758ed447d561ad422bf4ac92e75e7a1bdf53abf5
Lolital.gane71@gmail.com <you@example.com>
2024-03-20 22:53:17 +0100

    modification de showCovoiturage

--- views/covoiturages/showCovoiturage.php
+++ views/covoiturages/showCovoiturage.php
@@ -1,27 +1,26 @@
 <div class="container">
     <div class="row">
         <h1 class="mt-5">Covoiturage</h1>
-        <div class="col-md-8"> <!-- Colonne pour le texte -->
-            <p>Trajet proposé par <?php echo $conducteur;?></p>
+        <div class="col-md-8">
+        <p>Trajet proposé par <span style="color:#69BACE;"><strong><?php echo $conducteur;?></strong></span></p>
             <ul>
-                <li>Places disponibles :  <?php echo $covoit->getNbPlace();?></li>
-                <li>Prix par personne : <?php echo $covoit->getMontantParPers();?></li>
-                <li>Adresse de départ : <?php echo $covoit->getLieuDepart();?></li>
-                <li>Heure de départ : <?php echo $covoit->getHeureDepart();?></li>
+                <li class="mb-3">Places disponibles : <span style="color:#69BACE;"><strong><?php echo $covoit->getNbPlace();?></strong></span></li>
+                <li class="mb-3">Prix par personne : <span style="color:#69BACE;"><strong><?php echo $covoit->getMontantParPers();?> €</strong></span></li>
+                <li class="mb-3">Adresse de départ : <span style="color:#69BACE;"><strong><?php echo $covoit->getLieuDepart();?></strong></span></li>
+                <li class="mb-3">Heure de départ : <span style="color:#69BACE;"><strong><?php echo $covoit->getHeureDepart();?></strong></span></li>
             </ul>
-            <p>Information de contact : <?php echo $covoit->getInformationDeContact();?></p>
-            <p>Description : <?php echo $covoit->getDescriptif();?></p>
-            <p>Véhicule du capitaine : <?php echo $vehicule->getLibelle_vehicule();?></p>
-
-            <a class="btn btn-primary mb-5" href="?controller=evenements&action=showEvent&id_event=<?php echo $covoit->getIdEvent();?>">Retour</a>
+            <p>Information de contact : <span style="color:#69BACE;"><strong><?php echo $covoit->getInformationDeContact();?></strong></span></p>
+            <p>Description : <span style="color:#69BACE;"><strong><?php echo $covoit->getDescriptif();?></strong></span></p>
+            <p class="mb-5">Véhicule du capitaine : <span style="color:#69BACE;"><strong><?php echo $vehicule->getLibelle_vehicule();?></strong></span></p>
+            <a class="btn btn-primary mb-5 me-5" href="?controller=evenements&action=showEvent&id_event=<?php echo $covoit->getIdEvent();?>">Retour</a>
             <?php if($inscrit) {
-                echo '<a href="?controller=covoiturages&action=desinscriptionCovoiturage&id_covoiturage='.$covoit->getIdCovoiturage().'">Annuler la réservation</a>';
+                echo '<a class="ms-5 btn btn-primary mb-5" href="?controller=covoiturages&action=desinscriptionCovoiturage&id_covoiturage='.$covoit->getIdCovoiturage().'">Annuler la réservation</a>';
             } elseif(!$verif) {
-                echo '<a href="?controller=covoiturages&action=inscriptionCovoiturage&id_covoiturage='.$covoit->getIdCovoiturage().'">Réserver</a>';
+                echo '<a class="ms-5 btn btn-primary mb-5" href="?controller=covoiturages&action=inscriptionCovoiturage&id_covoiturage='.$covoit->getIdCovoiturage().'">Réserver</a>';
             }
             ?>
         </div>
-        <div class="col-md-4"> <!-- Colonne pour l'image -->
+        <div class="col-md-4">
             <img src="assets\img\carpool-5508006_1280.png">
         </div>
     </div>
